1b383d87781252f1af958099b5abb0c1f3b5c7a3
Suraj Thapa <surajthapart@gmail.com>
2024-07-14 12:19:31 +0545

    Routes added

--- e-learning/src/App.jsx
+++ e-learning/src/App.jsx
@@ -1,14 +1,28 @@
-import Nav from "./components/nav/Nav"
-import Home from "./components/home/home"
-import { Footer } from "./components/footer/Footer"
+import Nav from "./components/nav/Nav";
+import Home from "./components/home/home";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Footer } from "./components/footer/Footer";
+import About from "./components/AboutUs/About";
+import Contact from "./components/contact/Contact";
+import Log from "./components/Login/Log";
 function App() {
   return (
     <>
-      <Nav/>
-      <Home/>
-      <Footer/>
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/AboutUs" element={<About />} />
+          <Route path="/Project" element={<Log />} />
+          <Route path="*" element={<h1> Page Not Found</h1>} />
+        </Routes>
+      </Router>
+     
+      
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- e-learning/src/components/nav/Nav.jsx
+++ e-learning/src/components/nav/Nav.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {FaBars, FaBook } from "react-icons/fa";
 import Dropdown from "./Dropdown";
+import { Link , NavLink } from "react-router-dom";
 const Nav = () => {
 
   const subject = ["Maths" , "Science", "Social"]
@@ -18,24 +19,24 @@ const Nav = () => {
         
           <nav className="hidden sm:flex gap-4 sm:gap-5 pr-2 items-center">
           <Dropdown value = "Courses" options = {subject}/>
-            <a
-              href="#"
+            <NavLink
+              to="/"
               className="text-sm lg:text-sm font-medium hover:underline underline-offset-4"
             >
               Home
-            </a>
-            <a
-              href="#"
+            </NavLink>
+            <NavLink
+              to="/AboutUs"
               className="text-sm lg:text-sm font-medium hover:underline underline-offset-4"
             >
               About
-            </a>
-            <a
-              href="#"
+            </NavLink>
+            <NavLink
+              to="/Contact"
               className="text-sm lg:text-sm font-medium hover:underline underline-offset-4"
             >
               Contact Us
-            </a>
+            </NavLink>
           </nav>
           <button className="bg-[#0056D2] px-4 py-2 rounded-2xl text-white hover:bg-[#0041A8] transition duration-500">Sign up</button>
         </div>
